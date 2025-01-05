a60dcc980a6d36c7f05ec5b161f572d50a8bb952
Hantao Cui <cuihantao@gmail.com>
2025-01-05 18:47:29 -0500

    Zoom in preview properly.

--- assets/js/zoom.js
+++ assets/js/zoom.js
@@ -2,5 +2,8 @@
 $(document).ready(function () {
   medium_zoom = mediumZoom("[data-zoomable]", {
     background: getComputedStyle(document.documentElement).getPropertyValue("--global-bg-color") + "ee", // + 'ee' for trasparency.
+    margin: 24,
+    scrollOffset: 0,
+    container: null,  // Let medium-zoom handle container automatically
   });
 });
